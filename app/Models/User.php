1ee90bd75cfd3843485ad8783e28c7a63101ea63
shahboz007 <olmasovshahboz007@gmail.com>
2023-02-20 09:34:15 +0500

    Application model done

--- app/Models/User.php
+++ app/Models/User.php
@@ -31,4 +31,6 @@ class User extends Authenticatable
     ];
 
     public function role(){return $this->belongsTo(Role::class);}
+
+    public function applications(){return $this->hasMany(Application::class)}
 }
